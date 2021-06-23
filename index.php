043529e40b6c21b936c9876859fb6d5089578650
Michael Scocco <scocco.michael87@gmail.com>
2021-06-23 08:54:49 +0200

    snack 6

--- index.php
+++ index.php
@@ -30,6 +30,18 @@ Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .box-grigio{
+            width: 100px;
+            height: 100px;
+            background-color: gray;
+        }
+        .box-verde{
+            width: 100px;
+            height: 100px;
+            background-color: green;
+        }
+    </style>
 </head>
 <body>
 <h1>
@@ -89,6 +101,68 @@ Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno 
         }
     }
 
+    ?>
+
+</h1>
+
+<h1>
+    Snack 6
+    
+    <?php  
+    /*
+      Snack 6
+        Utilizzare questo array: https://pastebin.com/CkX3680A. 
+        Stampiamo il nostro array mettendo gli insegnanti in un 
+        rettangolo grigio e i PM in un rettangolo verde.
+    */
+        $db = [
+            'teachers' => [
+                [
+                    'name' => 'Michele',
+                    'lastname' => 'Papagni'
+                ],
+                [
+                    'name' => 'Fabio',
+                    'lastname' => 'Forghieri'
+                ]
+            ],
+            'pm' => [
+                [
+                    'name' => 'Roberto',
+                    'lastname' => 'Marazzini'
+                ],
+                [
+                    'name' => 'Federico',
+                    'lastname' => 'Pellegrini'
+                ]
+            ]
+        ];
+        
+        $teachers = $db['teachers'];
+        $pms = $db['pm'];
+
+        var_dump($teachers);
+        echo '<br>';
+        var_dump($pms);
+
+
+        for ($i=0; $i < count($teachers); $i++) { 
+            # code...
+            $teacher = $teachers[$i];
+
+            echo '<div class="box-grigio">' 
+            . $teacher['name'] . ' ' . $teacher['lastname']
+            . '</div>';
+        }
+
+        for ($i=0; $i < count($pms); $i++) { 
+            # code...
+            $pm = $pms[$i];
+
+            echo '<div class="box-verde">' 
+            . $pm['name'] . ' ' . $pm['lastname']
+            . '</div>';
+        }
     ?>
 
 </h1>
